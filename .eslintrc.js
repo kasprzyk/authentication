8728dc953adfe44ae1c35c4db46780430c015336
kasprzyk <piotkas1@gmail.com>
2020-06-09 17:17:51 +0200

    RegisterUser

--- .eslintrc.js
+++ .eslintrc.js
@@ -3,19 +3,17 @@ module.exports = {
   env: {
     node: true
   },
-  extends: ["plugin:vue/recommended", "standard"],
-  rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "error" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "error" : "off",
-    "vue/max-attributes-per-line": ["error", {
-      "singleline": 4,
-      "multiline": {
-        "max": 1,
-        "allowFirstLine": false
-      }
-    }]
-  },
+  extends: [
+    'plugin:vue/essential',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+    '@vue/prettier'
+  ],
   parserOptions: {
-    parser: "babel-eslint"
+    parser: 'babel-eslint'
+  },
+  rules: {
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   }
-};
+}
