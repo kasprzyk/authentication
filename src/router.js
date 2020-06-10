4c0a080246d79bd6af2bf2dc8afdf541144bd1a3
kasprzyk <piotkas1@gmail.com>
2020-06-10 10:30:02 +0200

    SET_USER_DATA, register added to store

--- src/router.js
+++ src/router.js
@@ -1,7 +1,9 @@
+/* eslint-disable */
 import Vue from 'vue'
 import Router from 'vue-router'
 import Home from './views/Home.vue'
 import Dashboard from './views/Dashboard.vue'
+import RegisterUser from './views/RegisterUser.vue'
 
 Vue.use(Router)
 
@@ -12,14 +14,19 @@ const router = new Router({
     {
       path: '/',
       name: 'home',
-      component: Home
+      component: Home,
     },
     {
       path: '/dashboard',
       name: 'dashboard',
-      component: Dashboard
-    }
-  ]
+      component: Dashboard,
+    },
+    {
+      path: '/register',
+      name: 'register',
+      component: RegisterUser,
+    },
+  ],
 })
 
 export default router
